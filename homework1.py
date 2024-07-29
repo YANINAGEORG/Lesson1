63df340237f1a2f82762c26f2664c4e6f02410dc
Yanina Georg <starushka@mail.ru>
2024-07-29 09:51:34 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,19 @@
+# Lesson 2 str
+
+# 1st program
+example = 'Cosmopolitan'
+
+# 2nd program
+print(example[0])
+
+# 3rd program
+print(example[-1])
+
+# 4th program
+print(example[6:])
+
+# 5th program
+print(example[::-1])
+
+# 6th program
+print(example[1::2])
