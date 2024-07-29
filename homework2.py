0745a903e8fdecf81b302d2d901664af52daae2f
Yanina Georg <starushka@mail.ru>
2024-07-29 14:52:47 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,14 @@
+# 1st program
+number_finished_jobs = 12
+
+# 2nd program
+number_hours_spent = 1.5
+
+# 3rd program
+course_name = 'Python'
+
+# 4th program
+time_for_one_task = (number_hours_spent / number_finished_jobs)
+
+# 5th program
+print('Курс:',course_name,',','всего задач:',number_finished_jobs,', затрачено часов: ',number_hours_spent,', среднее время выполнения ',time_for_one_task)
