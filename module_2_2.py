042b65b9ffb230960fd60310ffffbafae73eec1b
Yanina Georg <starushka@mail.ru>
2024-08-04 17:01:44 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,10 @@
+first = input('Введите первое число - ')
+second = input('Введите второе число - ')
+third = input('Введите третье число - ')
+
+if first != second and second != third and first != third:
+    print('Количество одинаковых чисел - 0')
+if (first == second and first != third) or (second == third and first != second):
+    print('Количество одинаковых чисел - 2')
+if first == second and second == third:
+    print('Количество одинаковых чисел - 3')
