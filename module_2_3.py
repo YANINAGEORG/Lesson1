596e4e55081372f94f8c19755aeb6eb31d1a376c
Yanina Georg <starushka@mail.ru>
2024-08-05 16:52:57 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,15 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]  # исходный список
+start = 0  # начальный индекс
+number = int(len(my_list)) # конечный индекс
+
+while my_list[start] >= 0 and start < number:
+    print(my_list[start])
+    start = start + 1
+else:
+    if my_list[start] < 0:
+        print('Конец цикла, найдено отрицательное число -', my_list[start])
+    if start == number:
+        print('Конец цикла - отрицательных чисел не найдено')
+
+
+
