ba0006b84ef00ed230ec06e47ff4ff74844079b4
Yanina Georg <starushka@mail.ru>
2024-08-06 11:58:55 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,15 +1,16 @@
-my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]  # исходный список
-start = 0  # начальный индекс
-number = int(len(my_list)) # конечный индекс
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]               # исходный список
+start = 0                                                            # начальный индекс
+number = int(len(my_list))                                           # конечный индекс
 
 while my_list[start] >= 0 and start < number:
-    print(my_list[start])
-    start = start + 1
+    if my_list[start] != 0:
+        print(my_list[start])
+        start = start + 1
+    else:
+        start = start + 1                           # ноль не выводится на печать, но не останавливает цикл
 else:
     if my_list[start] < 0:
         print('Конец цикла, найдено отрицательное число -', my_list[start])
     if start == number:
         print('Конец цикла - отрицательных чисел не найдено')
-
-
 
