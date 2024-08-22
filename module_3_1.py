0baf3dd8de2674566253b7e92816596e4b460918
Yanina Georg <starushka@mail.ru>
2024-08-22 21:29:17 +0300

    Add files via upload

--- module_3_1.py
+++ module_3_1.py
@@ -1,24 +1,27 @@
 calls = 0
-def count_calls(calls):  # функция счетчик
+def count_calls():  # функция счетчик
+    global calls
     calls += 1
 
 def string_info(string):
-    return len(string), string.upper(), string.lower()
     count_calls()
+    return len(string), string.upper(), string.lower()
+
+
 
 def is_constains(string, list_to_search):
+    count_calls()
     if string.lower() in list(map(str.lower, list_to_search)):
         print(string, list_to_search, True)
     else:
         print(string, list_to_search, False)
-    count_calls()
 
 
 print(string_info('Refrigerator'))
 print(string_info('ElefANT'))
 
-print(is_constains('Yana', ['Bob', 'APPYANA', 'yaNa']))
-print(is_constains('YaniNA', ['Boby', 'APPYANINA', 'yaNa']))
+is_constains('Yana', ['Bob', 'APPYANA', 'yaNa'])
+is_constains('YaniNA', ['Boby', 'APPYANINA', 'yaNa'])
 
 print(calls)
 
