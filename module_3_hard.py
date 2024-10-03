5be250ad7080c4b74f23617b2b09f0dfc6701ec8
Yanina Georg <starushka@mail.ru>
2024-10-03 12:11:54 +0300

    Add files via upload

--- module_3_hard.py
+++ module_3_hard.py
@@ -1,33 +1,22 @@
-def calculate_structure_sum(data_structure):
-    data_structure = []
+def calculate_structure_sum(*data_structure):
     sum_ = 0
-
     for i in data_structure:
+        if isinstance(i, (int, float)):
+            sum_ += i
+        if isinstance(i, str):
+            sum_ += len(i)
+        if isinstance(i, dict):
+            sum_ += calculate_structure_sum(*i.items())
+        if isinstance(i, (list, tuple, set)):
+            sum_ += calculate_structure_sum(*i)
+        if i is None:
+            pass
+    return sum_
 
-        while data_structure[i] is not int():
-
-            if type(i) is str():
-                i = len(i)
-                return i
-
-            if type(i) is list() or type(i) is tuple():
-                data_structure.append(*i)
-                return i
-
-            if type(i) is dict():
-                data_structure.append(**i)
-                return i
-
-        data_structure = sum(data_structure)
-
-data_structure = [
-  [1, 2, 3],
-  {'a': 4, 'b': 5},
-  (6, {'cube': 7, 'drum': 8}),
-  "Hello",
-  ((), [{(2, 'Urban', ('Urban2', 35))}])
-]
-
-result = calculate_structure_sum(data_structure)
-print(result)
-
+print (calculate_structure_sum([
+[1, 2, 3],
+{'a': 4, 'b': 5},
+(6, {'cube': 7, 'drum': 8}),
+"Hello",
+((), [{(2, 'Urban', ('Urban2', 35))}])
+]))
